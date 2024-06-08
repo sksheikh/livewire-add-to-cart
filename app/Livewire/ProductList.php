d943c7290fd4401c10c506742bc1cfca6ed3c11a
Skuddin <sheikh@nextgenitltd.com>
2024-06-08 11:44:39 +0600

    created model and migration for Cart

--- app/Livewire/ProductList.php
+++ app/Livewire/ProductList.php
@@ -8,9 +8,18 @@ use Livewire\Component;
 class ProductList extends Component
 {
     public $products;
+
     public function render()
     {
         $this->products = Product::get();
         return view('livewire.product-list');
+    }
+
+    public function addToCart($id){
+        if(auth()->user()){
+
+        }else{
+            return redirect()->route('login');
+        }
     }
 }
--- resources/views/livewire/product-list.blade.php
+++ resources/views/livewire/product-list.blade.php
@@ -12,7 +12,11 @@
 
                     <div class="flex items-center justify-between">
                         <span class="text-3xl font-bold text-gray-900 dark:text-white">{{ $product->price }} BDT</span>
-                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                        <button
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            wire:click="addToCart({{ $product->id }})"
+                            >Add to cart
+                        </button>
                     </div>
                 </div>
             </div>
